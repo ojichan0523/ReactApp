dedc23c23686e9493950def5bb1f45db09290fc6
ojichan0523 <yu2.yanagisawa@gmail.com>
2023-03-08 22:17:14 +0900

    only nasa-app

--- DogAlbum/src/api.ts
+++
@@ -1,7 +0,0 @@
-export async function fetchImages(breed: any) {
-  const response = await fetch(
-    `https://dog.ceo/api/breed/${breed}/images/random/12`
-  );
-  const data = await response.json();
-  return data.message;
-}
